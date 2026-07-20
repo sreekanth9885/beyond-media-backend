ec0574833f6278f103c49ccb461ecfc54487b9bf
Srikanth Bethi <srikanth.bethi01@gmail.com>
2026-07-20 21:54:01 +0530

    404 issue

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,4 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -6,6 +6,8 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
 export default pool;
