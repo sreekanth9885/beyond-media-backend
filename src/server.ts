4cc3c78c56677702cca212b92d8f5a75d9a8781c
Srikanth Bethi <srikanth.bethi01@gmail.com>
2026-07-20 17:52:56 +0530

    simple login api

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import db from "./config/db";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -32,7 +33,48 @@ app.get("/health", (_req, res) => {
     timestamp: new Date(),
   });
 });
+// LOGIN API
+app.post("/api/auth/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and Password are required",
+      });
+    }
+
+    const [rows]: any = await db.execute(
+      `SELECT id, name, email
+       FROM users
+       WHERE email = ?
+       AND password = ?
+       LIMIT 1`,
+      [email, password],
+    );
+
+    if (rows.length === 0) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    return res.json({
+      success: true,
+      message: "Login successful",
+      user: rows[0],
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+});
 app.listen(Number(PORT), "0.0.0.0", () => {
   console.log(`Server running on ${PORT}`);
 });
