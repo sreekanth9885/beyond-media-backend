71324343356e10cdc4f18d25903ed4a1c6ffefde
Srikanth Bethi <srikanth.bethi01@gmail.com>
2026-07-25 12:33:13 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import corsMiddleware from "./middleware/cors";
 import routes from "./routes";
 import path from "path";
+import newsRoutes from "./module/news/news.routes";
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use(express.json());
 
 app.use(routes);
+app.use(newsRoutes);
 
 export default app;
