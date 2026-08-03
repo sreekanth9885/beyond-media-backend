c9bb481d345a300c7a68093feb0a50afd071411e
Srikanth Bethi <srikanth.bethi01@gmail.com>
2026-08-03 18:22:49 +0530

    Update health.routes.ts

--- src/routes/health.routes.ts
+++ src/routes/health.routes.ts
@@ -5,7 +5,7 @@ const router = Router();
 router.get("/", (_req, res) => {
   res.json({
     success: true,
-    message: "Beyond Media API is Up and running 🚀",
+    message: "Beyond Media API is Up and Running 🚀",
   });
 });
 
