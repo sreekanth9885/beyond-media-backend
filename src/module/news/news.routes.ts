d165819d0eae2d715809f360fd65c9f5259c6271
sreekanth9885 <srikanth.bethi01@gmail.com>
2026-08-13 12:19:49 +0530

    Update news.routes.ts

--- src/module/news/news.routes.ts
+++ src/module/news/news.routes.ts
@@ -12,11 +12,12 @@ import {
   changeNewsStatus,
   getNewsDetails,
 } from "./news.controller";
+import { authenticate } from "../../middleware/authenticate";
 
 const router = Router();
 
 // Create
-router.post("/", upload.single("featuredImage"), createNews);
+router.post("/", upload.single("featuredImage"), authenticate, createNews);
 
 // Admin list
 router.get("/", getNews);
@@ -29,10 +30,10 @@ router.get("/details/:slug", getNewsDetails);
 router.get("/id/:id", getSingleNews);
 
 // Update
-router.put("/:id", upload.single("featuredImage"), updateNews);
+router.put("/:id", upload.single("featuredImage"), authenticate, updateNews);
 router.patch("/:id/status", changeNewsStatus);
 
 // Delete
-router.delete("/:id", deleteNews);
+router.delete("/:id", authenticate, deleteNews);
 
 export default router;
